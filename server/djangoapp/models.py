15526e3af0695296cb95a9b9b7e86ec555d472f1
yafet <yafetbabi@gmail.com>
2024-07-24 04:43:58 -0400

    modules, populate views and setting solved

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -41,12 +41,10 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    
+    year = models.IntegerField(default=2023, validators=[
+        MaxValueValidator(2023),
+        MinValueValidator(2015)]
+        )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
